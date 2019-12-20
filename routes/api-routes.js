6dcead458c28da181200fd234bb2cc05259e4fc5
Hibah Shafi <hibahshafi.hs@gmail.com>
2019-12-19 18:47:29 -0600

    added the post method to have burgers created

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,8 +10,7 @@ var db = require("../models");
 
 module.exports = function (app) {
 
-    // GET route for getting all of the todos
-    app.get("/", function (req, res) {
+    app.get("/burgers", function (req, res) {
         // findAll returns all entries for a table when used with no options
         db.Burger.findAll({}).then(function (data) {
             // We have access to the todos as an argument inside of the callback function
@@ -21,5 +20,24 @@ module.exports = function (app) {
             res.render("index", burgerObject);
         });
     });
+
+    app.post("/api/burgers", function (req, res) {
+        // create takes an argument of an object describing the item we want to
+        // insert into our table. In this case we just we pass in an object with a text
+        // and complete property (req.body)
+        db.Burger.create({
+            burger: req.body.burger
+        }).then(function (data) {
+            // We have access to the new todo as an argument inside of the callback function
+            res.json(data);
+            console.log(data);
+        })
+            .catch(function (err) {
+                // Whenever a validation or flag fails, an error is thrown
+                // We can "catch" the error to prevent it from being "thrown", which could crash our node app
+                res.json(err);
+            });
+    });
+
 
 };
